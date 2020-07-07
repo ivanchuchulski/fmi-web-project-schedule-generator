5e805babdd35e499f8af66c4f50c00eddb4c1cb7
Ivan Chuchulski <zake6@abv.bg>
2020-07-07 13:00:38 +0300

    fix add event listener to buttons

--- script/schedule.js
+++ script/schedule.js
@@ -1,4 +1,3 @@
-// TODO : make event listeners on added buttons from loadEvents work
 // using the javascript immediately-invoked function expression (IIFE)
 (function () {
     window.onload = () => loadEvents();
@@ -9,13 +8,99 @@
 	
 	let preferences = [];
 
+	// 
 	let preferenceButtons = document.getElementsByClassName("preferenceButton");
 
 	for (let i = 0; i < preferenceButtons.length; i++) {
 		preferenceButtons[i].addEventListener("click", addToPreferences);
 	}
-
 })();
+
+
+function loadEvents() {
+	const LOAD_SCHEDULE_URL = "php/api.php/loadSchedule";
+	const LOAD_SCHEDULE_METHOD = "GET";
+
+	ajaxRequest(LOAD_SCHEDULE_URL, LOAD_SCHEDULE_METHOD);
+}
+
+function ajaxRequest(url, method, data) {
+	let xhr = new XMLHttpRequest();
+
+	xhr.addEventListener("load", () => requestHandler(xhr));
+
+	xhr.open(method, url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.send(data);
+}
+
+function requestHandler(xhr) {
+	let response = JSON.parse(xhr.responseText);
+
+	if (response.success) {
+		console.log('success load schedule');
+		drawEvents(response.data);
+    }
+    else {
+		console.log('error : load schedule');
+    }
+}
+
+function drawEvents(events) {
+	let eventParent = document.getElementById('event-list');
+	
+	let eventList = JSON.parse(events); 
+
+	console.log(JSON.stringify(eventList, null, 4));
+	console.log(eventList);
+
+	Object.keys(eventList).forEach((event) => {
+		let theme = eventList[event].theme;
+		let presentDate = eventList[event].presentDate;
+		let presenterName = eventList[event].presenterName;
+		let place = eventList[event].place;
+
+		// console.log("event : ");
+		// console.log(theme);
+		// console.log(presentDate);
+		// console.log(presenterName);
+		// console.log(place);
+		
+		let eventElement = document.createElement("div");
+		let details = document.createElement("div");
+		let timeinfo = document.createElement("div");
+		let preference = document.createElement("div");
+		let willGoButton = document.createElement("button");
+		let couldGoButton = document.createElement("button");
+
+		eventElement.className += "event";
+
+		details.className += "details";
+		timeinfo.className += "timeinfo";
+		preference.className += "preference";
+		willGoButton.className += "preferenceButton willAttend";
+		couldGoButton.className += "preferenceButton couldAttend";
+
+		details.innerHTML = `<p class="presenter">${presenterName}</p> <p class="theme">${theme}</p>`
+		timeinfo.innerHTML = `<p class="date">${presentDate}</p> <p class="presentationSite">${place}</p>`
+
+		willGoButton.innerText += "will attend";
+		couldGoButton.innerText += "could attend";
+
+		eventParent.appendChild(eventElement);
+		
+		eventElement.appendChild(details);
+		eventElement.appendChild(timeinfo);
+		eventElement.appendChild(preference);
+
+		preference.appendChild(willGoButton);
+		preference.appendChild(couldGoButton);
+
+		willGoButton.addEventListener("click", addToPreferences);
+		couldGoButton.addEventListener("click", addToPreferences);
+	});
+}
+
 
 // maybe not directly write to db when manipulating preferences, but add them to some list 
 // which is passed as parameter, and at clicking on 'generate personal' pass the list at once
@@ -33,7 +118,6 @@ function addToPreferences() {
         
 		return;
 	}
-
 
 	// if its not, then find its active sibiling and remove it 
 	let preferenceButtons = this.parentElement.getElementsByClassName("preferenceButton");
@@ -115,76 +199,6 @@ function updateButtonHighlight() {
 	}
 }
 
-function loadEvents() {
-	const LOAD_SCHEDULE_URL = "php/api.php/loadSchedule";
-	const LOAD_SCHEDULE_METHOD = "GET";
-
-	ajaxRequest(LOAD_SCHEDULE_URL, LOAD_SCHEDULE_METHOD);
-}
-
-function ajaxRequest(url, method, data) {
-	let xhr = new XMLHttpRequest();
-
-	xhr.addEventListener("load", () => requestHandler(xhr));
-
-	xhr.open(method, url, true);
-	xhr.setRequestHeader("Content-type", "application/json");
-	xhr.send(data);
-}
-
-function requestHandler(xhr) {
-	let response = JSON.parse(xhr.responseText);
-
-	if (response.success) {
-		console.log('success load schedule');
-		drawEvents(response.data);
-    }
-    else {
-		console.log('error : load schedule');
-    }
-}
-
-function drawEvents(events) {
-	let eventParent = document.getElementById('event-list');
-	
-	let eventList = JSON.parse(events); 
-
-	console.log(JSON.stringify(eventList, null, 4));
-	console.log(eventList);
-
-	Object.keys(eventList).forEach((event) => {
-		let theme = eventList[event].theme;
-		let presentDate = eventList[event].presentDate;
-		let presenterName = eventList[event].presenterName;
-		let place = eventList[event].place;
-
-		// console.log("event : ");
-		// console.log(theme);
-		// console.log(presentDate);
-		// console.log(presenterName);
-		// console.log(place);
-		
-		let eventElement = document.createElement("div");
-		let details = document.createElement("div");
-		let timeinfo = document.createElement("div");
-		let preference = document.createElement("div");
-
-		eventElement.className += "event";
-
-		details.className += "details";
-		timeinfo.className += "timeinfo";
-		preference.className += "preference";
-
-		details.innerHTML = `<p class="presenter">${presenterName}</p> <p class="theme">${theme}</p>`
-		timeinfo.innerHTML = `<p class="date">${presentDate}</p> <p class="presentationSite">${place}</p>`
-		preference.innerHTML = `<button class="preferenceButton willAttend">will attend</button> <button class="preferenceButton couldAttend">could attend</button>`
-
-		eventParent.appendChild(eventElement);
-		eventElement.appendChild(details);
-		eventElement.appendChild(timeinfo);
-		eventElement.appendChild(preference);
-	})
-}
 
 function logError(object) {
 	console.error("errors : ");
