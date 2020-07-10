7c5e826881ea56e66903169f300607200d34dd14
DanielDimitrovD <dakata619@gmail.com>
2020-07-10 14:28:35 +0300

    Finish filters by group and day of presentation

--- script/schedule.js
+++ script/schedule.js
@@ -29,20 +29,25 @@
 	let applyFilterButton = document
 	.getElementById("apply-filter")
 	.addEventListener("click", generateScheduleByFilters);
+
+
+	let resetFilterButton = document
+	.getElementById("reset-filter")
+	.addEventListener("click", removeFilters);
+
 })();
 
 function generateScheduleByFilters() {
+
 	let daySelectElement = document.getElementById("filter-by-day"); 
 	let groupSelectElement = document.getElementById("filter-by-group"); 
 
 	let filterByDay = daySelectElement.options[daySelectElement.selectedIndex].value;
-    let filterByGroup = groupSelectElement.options[groupSelectElement.selectedIndex].value;
-
-
-	// console.log(filterByDay.slice(-1));
-	// console.log(filterByGroup);
-	
+	let filterByGroup = groupSelectElement.options[groupSelectElement.selectedIndex].value;
+		
 	let events = document.getElementsByClassName("event");
+
+	displayAllEvents();
 
 	if (filterByDay) {
 		filterEventsByDay(events, filterByDay.slice(-1));
@@ -58,7 +63,7 @@ function filterEventsByDay(events, dayFilter) {
 		const event = events[index];
 		let eventDay = event.getElementsByClassName("day-number")[0].innerText.split(" ")[1];
 
-		console.log(eventDay);
+		//console.log(eventDay);
 
 		if (eventDay != dayFilter) {
 			hideEvent(event);
@@ -67,15 +72,49 @@ function filterEventsByDay(events, dayFilter) {
 }
 
 function hideEvent(event) {
-	event.className += " hide";
+	event.style.display = "none";
 }
 
-function showEvent(event) {
-	event.className = event.className.replace(" hide", "");
+
+function removeFilters() {
+		displayAllEvents();
+		removeSelectedFilters();
 }
 
-function filterEventsByGroup(events) {
-	
+function displayAllEvents() {
+
+	let daySelectElement = document.getElementById("filter-by-day"); 
+	let groupSelectElement = document.getElementById("filter-by-group"); 
+
+	let events = document.getElementsByClassName("event");
+	//console.log(events);
+
+	for (let index = 0; index < events.length; index++) {
+		let element = events[index];
+		console.log(element);
+		element.style.display = "flex";
+	}
+}
+
+function removeSelectedFilters() {
+	let daySelectElement = document.getElementById("filter-by-day"); 
+	let groupSelectElement = document.getElementById("filter-by-group"); 
+
+	daySelectElement.selectedIndex = 0;
+	groupSelectElement.selectedIndex = 0;
+}
+
+function filterEventsByGroup(events, groupFilter) {
+	for (let index = 0; index < events.length; index++) {
+		const event = events[index];
+		let eventGroup = event.getElementsByClassName("group-number")[0].innerText.split(" ")[1];
+
+		//console.log(eventDay);
+
+		if (eventGroup != groupFilter) {
+			hideEvent(event);
+		}
+	}
 }
 
 
