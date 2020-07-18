ba26360e5e2ff16563f1ad5dc273dcfbcaca6839
Viktor Hristov <hristov_98@yahoo.com>
2020-07-18 14:58:35 +0300

    Update document and add incorrect export

--- php/export_schedule_in_pdf.php
+++ php/export_schedule_in_pdf.php
@@ -7,18 +7,36 @@ function exportScheduleInPdf() {
 	try {
 		checkSessionSet();
 
-		$filename = "download_files/output.pdf";
+		$config = parse_ini_file("config/config.ini", true);
+		$presentationsFilePath = $config['presentations']['path'];
 
+		if (!file_exists($presentationsFilePath)) {
+			throw new Exception("грешка : файлът с презентациите не може да бъде намерен");
+		}
+
+		$presentationsJSONString = file_get_contents($presentationsFilePath);
+		$decodedPresentations = json_decode($presentationsJSONString, true);
 
 		$pdf = new FPDF();
-
 		$pdf->AddPage();
-		$pdf->SetFont('Arial', 'B', 16);
-
-		$pdf->Cell(40, 10, 'Hello World!');
+		$pdf->SetFont('Arial', 'B', 12);
+		
+		// does not support utf8?
+		foreach ($decodedPresentations as &$decodedPresentation) {
+			$pdf->Cell(200, 10, $decodedPresentation['theme'] 
+			. "\t" . $decodedPresentation['presentDate'] 
+			. "\t" . $decodedPresentation['dayNumber'],0,1);
+	
+			$pdf->Cell(200, 10, $decodedPresentation['presenterName'] 
+			. "\t" . $decodedPresentation['facultyNumber'] 
+			. "\t" . $decodedPresentation['groupNumber'],0,1);
+	
+			$pdf->Cell(200, 10, $decodedPresentation['place'],0,1);
+		}
 
 		// could use D option do force download at this point
 		// in this case remove all other code below
+		$filename = "download_files/output.pdf";
 		$pdf->Output("F", $filename);
 
 		// this forces the browser to download the file
