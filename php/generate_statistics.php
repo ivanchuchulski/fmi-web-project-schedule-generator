874894f5ee9cc6ea0464af41420299f748cd02a2
DanielDimitrovD <dakata619@gmail.com>
2020-07-21 17:28:52 +0300

    Handle statistics exceptions

--- php/generate_statistics.php
+++ php/generate_statistics.php
@@ -2,30 +2,42 @@
 
 require_once "statistics.php";
 
-function generateStatistics() {
+function generateStatistics()
+{
 
-	try {
-		checkSessionSet();
+    try {
+        checkSessionSet();
 
-		$stats = [];
-		$statistics = new Statistics();
+        $stats = [];
+        $statistics = new Statistics();
 
-		$stats += $statistics->getNumberOfUsers();
-		$stats += $statistics->getNumberOfPresentations();
-		$stats += $statistics->getNumberOfPreferences();
-		$stats += $statistics->getMaxNumberOfPreferencesFromAllUsers();
-		$stats += $statistics->getAverageNumberOfPreferences();
-		$stats += $statistics->getMostPreferredPresentation();
+        $stats += $statistics->getNumberOfUsers();
+        $stats += $statistics->getNumberOfPresentations();
+        $stats += $statistics->getNumberOfPreferences();
 
-		$topFivePresentations = $statistics->getTopFivePresentations();
+        $stats += $statistics->getMaxNumberOfPreferencesFromAllUsers();
+        if (is_null($stats['maxNumberOfPreference'])) {
+            $stats['maxNumberOfPreference'] = 0;
+        }
 
-		$response = ['success' => true, 'data' => $stats, 'topFivePresentations' => $topFivePresentations];
-		echo json_encode($response);
-	}
-	catch (Exception $exception) {
-		$response = ['success' => false, 'error' => $exception->getMessage()];
-		echo json_encode($response);
-	}
+        $stats += $statistics->getAverageNumberOfPreferences();
+        if (is_null($stats['averageNumberOfPreference'])) {
+            $stats['averageNumberOfPreference'] = 0;
+        }
+
+        $stats += $statistics->getMostPreferredPresentation();
+        if (is_null($stats['mostPreferredPresentation'])) {
+            $stats['mostPreferredPresentation'] = 0;
+        }
+
+        $topFivePresentations = $statistics->getTopFivePresentations();
+
+        $response = ['success' => true, 'data' => $stats, 'topFivePresentations' => $topFivePresentations];
+        echo json_encode($response);
+    } catch (Exception $exception) {
+        $response = ['success' => false, 'error' => $exception->getMessage()];
+        echo json_encode($response);
+    }
 
 }
 
