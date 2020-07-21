0e11db1b7738f545ae869d6205b8da712256a0d1
Viktor Hristov <hristov_98@yahoo.com>
2020-07-21 14:14:04 +0300

    Rename updatePersonalSchedule to update_personal_schedule

--- php/api.php
+++ php/api.php
@@ -11,7 +11,7 @@ require_once "logout.php";
 require_once "load_schedule.php";
 require_once "generate_personal_schedule.php";
 require_once "load_personal_schedule.php";
-require_once "updatePersonalSchedule.php";
+require_once "update_personal_schedule.php";
 require_once "export_full_schedule_to_csv.php";
 require_once "export_personal_schedule_to_csv.php";
 require_once "generateStatistics.php";
