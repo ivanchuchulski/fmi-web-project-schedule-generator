d2931e035bbf7a5ffb9d4ecdf084d80c4973d162
Ivan Chuchulski <zake6@abv.bg>
2020-07-21 15:26:17 +0300

    fix bug in statistics

--- php/generate_statistics.php
+++ php/generate_statistics.php
@@ -2,30 +2,30 @@
 
 require_once "statistics.php";
 
- function generateStatistics() {
+function generateStatistics() {
 
-	 try {
-		 checkSessionSet();
+	try {
+		checkSessionSet();
 
-		 $stats = [];
-		 $statistics = new Statistics();
+		$stats = [];
+		$statistics = new Statistics();
 
-		 $stats += $statistics->getNumberOfUsers();
-		 $stats += $statistics->getNumberOfPresentations();
-		 $stats += $statistics->getNumberOfPreferences();
-		 $stats += $statistics->getMaxNumberOfPreferencesFromAllUsers();
-		 $stats += $statistics->getAverageNumberOfPreferences();
-		 $stats += $statistics->getMostPreferredPresentation();
+		$stats += $statistics->getNumberOfUsers();
+		$stats += $statistics->getNumberOfPresentations();
+		$stats += $statistics->getNumberOfPreferences();
+		$stats += $statistics->getMaxNumberOfPreferencesFromAllUsers();
+		$stats += $statistics->getAverageNumberOfPreferences();
+		$stats += $statistics->getMostPreferredPresentation();
 
-		 $stats += $statistics->getTopFivePresentations();
+		$topFivePresentations = $statistics->getTopFivePresentations();
 
-		 $response = ['success' => true, 'data' => $stats];
-		 echo json_encode($response);
-	 }
-	 catch (Exception $exception) {
-		 $response = ['success' => false, 'error' => $exception->getMessage()];
-		 echo json_encode($response);
-	 }
+		$response = ['success' => true, 'data' => $stats, 'topFivePresentations' => $topFivePresentations];
+		echo json_encode($response);
+	}
+	catch (Exception $exception) {
+		$response = ['success' => false, 'error' => $exception->getMessage()];
+		echo json_encode($response);
+	}
 
 }
 
--- script/statistics.js
+++ script/statistics.js
@@ -55,7 +55,7 @@ function ajaxLoadHandler(xhr) {
 
     if (response.success) {
         console.log("success load statistics");
-        drawStatistics(response.data);
+        drawStatistics(response);
     } else {
         console.log("error : load statistics");
         displayMessage(
@@ -64,7 +64,11 @@ function ajaxLoadHandler(xhr) {
     }
 }
 
-function drawStatistics(statisticsList) {
+function drawStatistics(response) {
+    const TEXT_CENTER_CLASSNAME = "text-center";
+
+    let statisticsList = response.data;
+    let topPresentations = response.topFivePresentations;
 
     let numberUsers = statisticsList.numberOfUsers;
     let numberPresentations = statisticsList.numberOfPresentations;
@@ -75,37 +79,41 @@ function drawStatistics(statisticsList) {
 
     let userCountTableData = document.getElementById("user-count");
     userCountTableData.innerText += numberUsers;
+    userCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let presentationCountTableData = document.getElementById("presentation-count");
     presentationCountTableData.innerText += numberPresentations;
+    presentationCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let preferenceCountTableData = document.getElementById("preference-count");
     preferenceCountTableData.innerText += numberPreferences;
+    preferenceCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let maxPreferenceCountTableData = document.getElementById("max-preference-count");
     maxPreferenceCountTableData.innerText += maxNumberOfPreference;
+    maxPreferenceCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let averagePreferenceCountTableData = document.getElementById("average-preference-count");
     averagePreferenceCountTableData.innerText += averageNumberOfPreference;
+    averagePreferenceCountTableData.className += TEXT_CENTER_CLASSNAME;
 
     let mostPopularPresentationTableData = document.getElementById("most-popular-presentation");
     mostPopularPresentationTableData.innerText += mostPreferredPresentation;
+    mostPopularPresentationTableData.className += TEXT_CENTER_CLASSNAME;
 
+    let tbody = document.getElementById("statistics-top-five-body");
 
-	let array = ["first-place", "second-place", "third-place", "fourth-place", "fifth-place"];
-	for (let i = 0; i < array.length; i++) {
-		let iterationPresentation = " " + statisticsList[i].theme;
-		let iterationPlaceTableData = document.getElementById(array[i]);
-		iterationPlaceTableData.innerText += iterationPresentation;
-	}
+    let positionCounter = 1;
+    topPresentations.forEach(presentation => {
+        let tr = document.createElement("tr");
 
-	let arrayForCount = ["first-place-count", "second-place-count", "third-place-count", "fourth-place-count",
-		"fifth-place-count"];
-	for (let i = 0; i < arrayForCount.length ; i++) {
-		let iterationPresentation = " " + statisticsList[i].count;
-		let iterationPlaceTableData = document.getElementById(arrayForCount[i]);
-		iterationPlaceTableData.innerText += iterationPresentation;
-	}
+        tr.innerHTML += `<td>${positionCounter}. ${presentation.theme}</td>
+                        <td>${presentation.count} </td>`;
+                        
+        positionCounter++;
+
+        tbody.appendChild(tr);
+    });
 
 }
 
